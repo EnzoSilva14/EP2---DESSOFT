77d122433b10f4c1d6885aa03fcc981662701d76
EnzoSilva14 <enzodrbs@al.insper.ed.br>
2023-05-03 14:54:57 -0300

    Fazendo jogada

--- EP2_1.0.py
+++ EP2_1.0.py
@@ -24,3 +24,10 @@ def preenche_frota (frota, nome_navio, linha, coluna, orientacao, tamanho):
     else:
         frota[nome_navio].append(posicao)
     return frota
+
+def faz_jogada (tabuleiro, linha, coluna):
+    if tabuleiro[linha][coluna] == 1:
+        tabuleiro[linha][coluna] = 'X'
+    else:
+        tabuleiro[linha][coluna] = '-'
+    return tabuleiro
