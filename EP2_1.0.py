e2b3d707a4387e75c7a8df388dc870359662c4b8
joaocavf <joaovcferreira5@gmail.com>
2023-05-09 23:01:06 -0300

    Posicionando as frotas

--- EP2_1.0.py
+++ EP2_1.0.py
@@ -3,7 +3,7 @@ def define_posicoes(linha, coluna, orientacao, tamanho):
     if orientacao == 'vertical':
         for i in range (tamanho):
             posicao.append([(linha + i),coluna])
-
+            
     elif orientacao == 'horizontal':
         for i in range(tamanho):
             posicao.append([linha, (coluna + i)])
@@ -68,6 +68,7 @@ def afundados(frota,tabuleiro):
   return navios_afundados
 
 def posicao_valida(frota, linha, coluna, orientacao, tamanho):
+
     posicao = define_posicoes(linha, coluna, orientacao, tamanho)
 
     for par in posicao:
@@ -78,3 +79,53 @@ def posicao_valida(frota, linha, coluna, orientacao, tamanho):
                 if par in listinha:
                     return False
     return True
+
+frota = {
+    "porta-aviões":[],
+    "navio-tanque":[],
+    "contratorpedeiro":[],
+    "submarino": [],
+}
+
+dicio = {
+    "porta-aviões": [4],
+    "navio-tanque": [3,3],
+    "contratorpedeiro": [2,2,2],
+    "submarino": [1,1,1,1],
+
+}
+
+for navio in dicio:
+    for i in range(len(dicio[navio])):
+        tamanho = dicio[navio][i]
+        print('Insira as informações referentes ao navio {0} que possui tamanho {1}'.format(navio, tamanho))
+        linha = int(input('Linha: '))
+        coluna = int(input('Coluna: '))
+        if tamanho > 1:
+            oriente = int(input('[1] Vertical [2] Horizontal >'))
+            if oriente == 1:
+                orientacao = 'vertical'
+            else:
+                orientacao = 'horizontal'
+
+        valida = False
+        while valida == False:
+            
+            valida = posicao_valida(frota, linha, coluna, orientacao, tamanho)
+            if valida == True:
+                posicao = define_posicoes(linha, coluna, orientacao, tamanho)
+                frota[navio].append(posicao)
+        
+            else:
+                print('Esta posição não está válida!')
+                print('Insira as informações referentes ao navio {0} que possui tamanho {1}'.format(navio, tamanho))
+                linha = int(input('Linha: '))
+                coluna = int(input('Coluna: '))
+                if tamanho > 1:
+                    oriente = int(input('[1] Vertical [2] Horizontal >'))
+                    if oriente == 1:
+                        orientacao = 'vertical'
+                    else:
+                        orientacao = 'horizontal'                
+    
+print (frota)
